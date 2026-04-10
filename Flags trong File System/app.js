a3a9bc9add6174e453dea519ef549712ccfe6449
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-04-10 17:15:26 +0700

    FLAG dành cho việc ghi

--- Flags trong File System/app.js
+++ Flags trong File System/app.js
@@ -12,44 +12,61 @@
  *  - 'r+': mở file để cả đọc và ghi. Nếu file k tồn tại, sẽ xảy ra lỗi
  */
 
+
+// // Mở file
+// const file = fs.openSync("./test.txt", "r+");
+
+// // Thao tác
+// const buffer = Buffer.alloc(10); // Tạo ra một vùng nhớ (buffer) 10 bytes => Dùng để chứa dữ liệu đọc từ file
+
+// // Buffer là một vùng nhớ tạm (temporary memory) dùng để chứa dữ liệu nhị phân khi bạn đọc/ghi dữ liệu.
+
+// const bytesRead = fs.readSync(file, buffer, 0, buffer.length, 0)
+
+// /**
+//  * file: file descriptor (đã mở trước đó bằng fs.openSync)
+//  * buffer: nơi chứa dữ liệu đọc vào
+//  * 0: bắt đầu ghi vào buffer từ vị trí 0
+//  * buffer.length: đọc tối đa 10 bytes
+//  * 0: bắt đầu đọc từ vị trí byte thứ 0 trong file
+//  * => kết quả: bytesRead: số byte thực sự đọc được (có thể < 10)
+//  */
+// const data = buffer.toString('utf-8', 0, bytesRead);
+
+// /**
+//  * Convert dữ liệu trong buffer sang chuỗi
+//  * Chỉ lấy từ 0 → bytesRead (tránh lấy phần rác)
+//  * => In ra nội dung file (tối đa 10 ký tự đầu)
+//  */
+// console.log(data);
+
+// const newBuffer = Buffer.from('New Content'); // Tạo buffer chứa chuỗi "New Content"
+// fs.writeSync(file, newBuffer, 0, newBuffer.length, 0)
+
+// /**
+//  * Ghi dữ liệu từ buffer vào file
+//  * 0: bắt đầu đọc từ buffer từ vị trí 0
+//  * newBuffer.length: ghi toàn bộ nội dung
+//  * 0: ghi vào file từ vị trí byte 0
+//  */
+
+// // Đóng file
+// fs.closeSync(file)
+
+/**
+ * 2. Các FLAG dành cho việc ghi
+ *  - 'w': Mở file để ghi
+ *      - mở file đã tồn tại: Nội dung cũ sẽ bị xóa sạch (truncate) để ghi mới
+ *      - nếu file chưa tồn tại: Mở 1 file mới sẽ được tạo
+ *  - 'wx': tương tự như 'w' nhưng sẽ thất bại nếu file đã tồn tại (chế độ độc quyền)
+ *  - 'w+': mở file để cả đọc và ghi
+ *      - nếu file đã tồn tại: Nội dung cũ bị xóa
+ *      - nếu chưa có: tạo file mới
+ *  - 'wx+': tương tự như 'w+' nhưng sẽ thất bại nếu đường dẫn đã tồn tại
+ */
+
 const fs = require('fs');
 
-// Mở file
-const file = fs.openSync("./test.txt", "r+");
-
-// Thao tác
-const buffer = Buffer.alloc(10); // Tạo ra một vùng nhớ (buffer) 10 bytes => Dùng để chứa dữ liệu đọc từ file
-
-// Buffer là một vùng nhớ tạm (temporary memory) dùng để chứa dữ liệu nhị phân khi bạn đọc/ghi dữ liệu.
-
-const bytesRead = fs.readSync(file, buffer, 0, buffer.length, 0)
-
-/**
- * file: file descriptor (đã mở trước đó bằng fs.openSync)
- * buffer: nơi chứa dữ liệu đọc vào
- * 0: bắt đầu ghi vào buffer từ vị trí 0
- * buffer.length: đọc tối đa 10 bytes
- * 0: bắt đầu đọc từ vị trí byte thứ 0 trong file
- * => kết quả: bytesRead: số byte thực sự đọc được (có thể < 10)
- */
-const data = buffer.toString('utf-8', 0, bytesRead);
-
-/**
- * Convert dữ liệu trong buffer sang chuỗi
- * Chỉ lấy từ 0 → bytesRead (tránh lấy phần rác)
- * => In ra nội dung file (tối đa 10 ký tự đầu)
- */
-console.log(data);
-
-const newBuffer = Buffer.from('New Content'); // Tạo buffer chứa chuỗi "New Content"
-fs.writeSync(file, newBuffer, 0, newBuffer.length, 0)
-
-/**
- * Ghi dữ liệu từ buffer vào file
- * 0: bắt đầu đọc từ buffer từ vị trí 0
- * newBuffer.length: ghi toàn bộ nội dung
- * 0: ghi vào file từ vị trí byte 0
- */
-
-// Đóng file
-fs.closeSync(file)
+fs.writeFileSync('./test12.txt', "new content", {
+    flag: 'w'
+});
