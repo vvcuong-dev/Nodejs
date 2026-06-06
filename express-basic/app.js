32512c45f96bc01b7b86cc0441cda256096be773
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-06-06 23:02:31 +0700

    Upload file Express - Cấu hình và upload 1 file

--- express-basic/app.js
+++ express-basic/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import userRouter from './routers/user-route.js';
 import morgan from 'morgan';
-import 'express-async-errors';
+import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
@@ -23,6 +25,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads'))); 
+
+/**
+ * app.use('/uploads', express.static(path.join(__dirname, 'public/uploads'))); 
+ *  - nghĩa là khi có yêu cầu đến đường dẫn bắt đầu bằng /uploads, Express sẽ tìm kiếm file trong thư mục public/uploads.
+ *  - Ví dụ: nếu có yêu cầu GET /uploads/123e4567-e89b-12d3-a456-426614174000.jpg, Express sẽ tìm file public/uploads/123e4567-e89b-12d3-a456-426614174000.jpg và trả về cho client.
+ *  - Điều này cho phép bạn truy cập trực tiếp các file đã được tải lên thông qua URL mà không cần phải tạo route riêng biệt để phục vụ từng file.
+ *  - tham số '/uploads' ở đầu tiên của app.use() chỉ định rằng middleware này sẽ được áp dụng cho tất cả các yêu cầu bắt đầu bằng /uploads. 
+ *    hiểu đơn giản là khi có yêu cầu đến đường dẫn bắt đầu bằng /uploads, Express sẽ sử dụng middleware express.static để tìm kiếm file trong thư mục public/uploads 
+ *    và trả về cho client nếu tìm thấy.
+ */
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -41,6 +54,38 @@ const myPromise = () => Promise.reject(new Error('This is a test error from myPr
 app.get('/debug', async (req, res) => {
    const data = await myPromise();
    throw new Error('This is a test error from /debug route.');
+});
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, 'public/uploads/');
+    },
+    filename: (req, file, callback) => {
+        const ext = path.extname(file.originalname); // lấy đuôi .jpg, .png...
+        const newFilename = crypto.randomUUID() + ext; // tên ngẫu nhiên + đuôi VD: 123e4567-e89b-12d3-a456-426614174000.jpg
+        callback(null, newFilename);
+    }
+});
+
+// 2. Tao middleware multer với cấu hình storage đã định nghĩa ở trên
+const upload = multer({ storage: storage });
+
+
+app.post('/uploads', upload.single('image'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ success: false, message: 'No file uploaded.' });
+    }
+
+    res.send({ success: true, message: 'File uploaded successfully.', filename:  `http://localhost:3000/uploads/${req.file.filename}` });
+});
+
+app.get('/uploads', (req, res) => {
+    res.send(
+        `<form action="/uploads" method="post" enctype="multipart/form-data">
+            <input type="file" name="image" />
+            <button type="submit">Upload</button>
+        </form>`
+    )
 });
 
 app.use((req, res, next) => {
