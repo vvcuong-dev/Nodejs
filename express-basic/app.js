b51ab96f7df1d5da217656f58f81b4c34281be0b
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-06-06 09:15:58 +0700

    Routing trong Express - Phần 1

--- express-basic/app.js
+++ express-basic/app.js
@@ -11,17 +11,30 @@ app.get('/about', (req, res) => {
     res.send('This is the about page.');
 });
 
-app.get('/contact', (req, res) => {
-    res.json([
-        {
-            name: 'John Doe',
-            email: 'abc@example.com'
-        },
-        {
-            name: 'Jane Doe',
-            email: 'jane@gmail.com'
-        }
-    ])
+app.get('/users', (req, res) => {
+    res.send(`
+        <h1>Users List</h1>
+        <form action="/users" method="POST">
+            <input type="text" name="username" placeholder="Enter username" required>
+            <button type="submit">Create User</button>
+        </form>
+        `);
+});
+
+app.post('/users', (req, res) => {
+    res.send('User created!');
+});
+
+// PUT: là cập nhật một tài nguyên đã tồn tại (tất cả thông tin của tài nguyên sẽ bị thay thế bằng thông tin mới)
+// PATCH: là cập nhật một phần của tài nguyên đã tồn tại (thông tin của tài nguyên sẽ được giữ lại nếu không được cung cấp trong yêu cầu cập nhật)
+
+app.put('/users/1', (req, res) => {
+    res.send(`User with ID 1has been updated!`);
+});
+
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    res.send(`User with ID ${userId}`);
 });
 
 app.listen(port, () => {
