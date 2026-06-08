574dd4a3994d8cff9bcb4756c902f90c05ae323b
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-06-08 14:53:59 +0700

    Kết nối NodeJS với Mysql thông qua mysql2 Driver

--- express-typescript/src/app.ts
+++ express-typescript/src/app.ts
@@ -1,20 +1,26 @@
+import dotenv from 'dotenv';
 import express, { Application, Request, Response} from 'express';
-import path from 'path';
-import fs from 'fs';
+import mysql from 'mysql2/promise';
 
 const app: Application = express();
 const port: number = 3000;
-const dataPath: string = path.join(__dirname, 'data', "db.json");
+dotenv.config();
 
-type User = {
-    name: string;
-    id: string;
-}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
+    const connection = await mysql.createConnection({
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USERNAME || 'root',
+        password: process.env.DB_PASSWORD || '123456',
+        port: Number(process.env.DB_PORT) || 3306,
+        database: process.env.DB_NAME || 'nodejs'
+    });
+    const [rows] = await connection.execute('SELECT * FROM users');
+    console.log(rows);
+
     res.send('Hello, World!');
 });
 
@@ -22,40 +28,6 @@ app.get('/about', (req: Request, res: Response) => {
     res.send('This is an Express server written in TypeScript.');
 });
 
-app.get('/users', (req: Request, res: Response) => {
-    const data = fs.readFileSync(dataPath, 'utf-8');
-    const users = JSON.parse(data);
-
-    res.send(`<h1>List of Users</h1>
-    <a href="/users/create">Create User</a>
-    <ul>
-        ${users.map((user: User) => `<li>${user.name} - ${user.id}</li>`).join('')}
-    </ul>
-        
-    `);
-});
-
-app.get('/users/create', (req: Request, res: Response) => {
-    res.send(`
-        <h1>Create User</h1>
-        <form action="/users" method="POST">
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name" required>
-            <button type="submit">Create User</button>
-        </form>
-        `);
-});
-
-
-app.post('/users', (req: Request, res: Response) => {
-    const data: User[] = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
-    data.push({
-        id: crypto.randomUUID(),
-        name: req.body.name
-    });
-    fs.writeFileSync(dataPath, JSON.stringify(data));
-    res.redirect('/users');
-});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
