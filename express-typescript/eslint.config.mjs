c213c80770428fd4b9e7920e37bcbd82ebc7c849
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-06-08 13:59:40 +0700

    Tích hợp Eslint vào dự án NodeJS + TypeScript

---
+++ express-typescript/eslint.config.mjs
@@ -0,0 +1,19 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            'prefer-const': 'error', // Yêu cầu sử dụng const thay vì let nếu biến không được gán lại
+             'no-unused-vars': 'warn', // Cảnh báo khi có biến được khai báo nhưng không sử dụng
+             '@typescript-eslint/no-unused-vars': 'warn', // Cảnh báo khi có biến TypeScript được khai báo nhưng không sử dụng
+              '@typescript-eslint/no-explicit-any': 'warn' // Tắt cảnh báo khi sử dụng kiểu any trong TypeScript
+        },
+    },
+    {
+        files: ['src/**/*.ts'], // Áp dụng quy tắc này chỉ cho các file TypeScript
+        ignores: ['node_modules/**', 'dist/**'], // Bỏ qua thư mục node_modules và dist
+    }
+);
