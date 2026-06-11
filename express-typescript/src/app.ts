0b4aaad25dcfa1b23c0b95e21d3faa71219c8d83
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-06-11 14:44:49 +0700

    Prisma Queries - CRUD - Phần 2

--- express-typescript/src/app.ts
+++ express-typescript/src/app.ts
@@ -15,17 +15,56 @@ app.get('/', async (req: Request, res: Response) => {
 });
 
 app.get('/users', async (req: Request, res: Response) => {
-    const users = await prisma.user.findUnique({
-        where: {
-            email: 'john.doe@example.com', 
-        },
-    });
-    res.json(users); // trả về null nếu không tìm thấy user nào có email trên, hoặc trả về đối tượng user nếu tìm thấy.
+   const users = await prisma.user.updateMany({
+    where: {
+        name: {
+            contains: "John" 
+            // nghĩa là tìm kiếm tất cả các user có tên chứa chuỗi "John". Ví dụ: "John Doe", "Johnny", "Johnson" đều sẽ được tìm thấy. 
+            // Nếu bạn muốn tìm kiếm chính xác tên "John", bạn có thể sử dụng equals thay vì contains, như sau: name: { equals: "John"
+        }
+    },
+    data: {
+        name: "Cường đẹp trai"
+    }
+   })
 
-    
+   res.json(users);
+
     // findFirst là tìm kiếm một user đầu tiên có tên là "John Doe", nếu có nhiều user có tên này thì chỉ trả về một user đầu tiên được tìm thấy. 
     // Nếu không tìm thấy user nào có tên "John Doe", thì sẽ trả về null.
 });
+
+
+app.get('/users-upsert', async (req: Request, res: Response) => {
+
+     const user = await prisma.user.upsert({
+        where: { 
+            email: "john.doe@example.com" 
+        },
+        update: { 
+            name: "Ahihi 11111",
+        },
+        create: { 
+            email: "john.doe@example.com", 
+            name: "Ahihi",
+            password: "123456"
+        }
+    });
+    res.json(user);
+});
+
+
+app.get('/users-delete', async (req: Request, res: Response) => {
+    
+    const user = await prisma.user.delete({
+        where: { 
+            id: 1,
+        },
+    });
+    res.json(user);
+});
+
+
 
 
 app.get('/about', (req: Request, res: Response) => {
