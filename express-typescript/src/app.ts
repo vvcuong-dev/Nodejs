f6365284c55b1c206872e23e1f0da7ce759d432e
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-06-11 14:28:39 +0700

    Prisma Queries - CRUD - Phần 1

--- express-typescript/src/app.ts
+++ express-typescript/src/app.ts
@@ -11,16 +11,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', async (req: Request, res: Response) => {
-    const user = await prisma.user.create({
-        data: {
-            name: 'John Doe',
-            email: "cuongvu@example.com",
-            password: "password123"
-        }
-    });
-
-    res.json(user);
+    res.send('Hello, World!');
 });
+
+app.get('/users', async (req: Request, res: Response) => {
+    const users = await prisma.user.findUnique({
+        where: {
+            email: 'john.doe@example.com', 
+        },
+    });
+    res.json(users); // trả về null nếu không tìm thấy user nào có email trên, hoặc trả về đối tượng user nếu tìm thấy.
+
+    
+    // findFirst là tìm kiếm một user đầu tiên có tên là "John Doe", nếu có nhiều user có tên này thì chỉ trả về một user đầu tiên được tìm thấy. 
+    // Nếu không tìm thấy user nào có tên "John Doe", thì sẽ trả về null.
+});
+
 
 app.get('/about', (req: Request, res: Response) => {
     res.send('This is an Express server written in TypeScript.');
