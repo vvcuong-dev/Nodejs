77e6bf47475c4da0519731c55d60a790c89020e1
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-06-15 21:12:36 +0700

    Template engines là gì, setup Ejs với Express

---
+++ Note/aggregation-grouping.ts
@@ -0,0 +1,6 @@
+// Prisma Queries - Aggregation, Grouping
+
+/**
+ * Trong prisma, aggregation và grouping là các kỹ thuật được sử dụng để thực hiện các phép toán tổng hợp trên dữ liệu trong cơ sở dữ liệu. giúp bạn thực hiện các phép toán như đếm, 
+ *  tính tổng, trung bình, giá trị lớn nhất, giá trị nhỏ nhất và nhiều hơn nữa trên các tập dữ liệu. Phép tính toán thống kê trực tiếp từ DB thay vì phải lấy tất cả bản ghi về rồi 
+ *  tính toán trên ứng dụng, giúp tăng hiệu suất và giảm tải cho ứng dụng của bạn. */
