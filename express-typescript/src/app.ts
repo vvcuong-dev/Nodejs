de501a90badcbb6acd43dfc21f6cb9d40870eafe
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-06-15 22:03:11 +0700

    Layout Ejs - Phần 1

--- express-typescript/src/app.ts
+++ express-typescript/src/app.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
-import express, { Application, Request, Response} from 'express';
+import express, { Application, NextFunction, Request, Response} from 'express';
 import path from 'path';
-// import { prisma } from './lib/prisma';
+import expressLayouts from 'express-ejs-layouts';
 
 const app: Application = express();
 const port: number = 3000;
@@ -10,19 +10,44 @@ dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('layout', 'layouts/main.layout.ejs');
+app.use(expressLayouts);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const url = req.url;
+    if (url.startsWith('/admin')) {
+        app.set("layout", 'layouts/admin.layout.ejs');
+    }
+    next();
+});
+
 
 app.get('/', async (req: Request, res: Response) => {
     const title = '<i>Welcome to Express with TypeScript!</i>';
     const status = false;
+    const users = [
+        { id: 1, name: 'Alice', email: 'alice@example.com' },
+        { id: 2, name: 'Bob', email: 'bob@example.com' },
+        { id: 3, name: 'Charlie', email: 'charlie@example.com' }
+    ]
+
     const utils = {
         doSomething: () => {
             return 'Doing something...';
         }
     }
 
-    res.render('home', { title, status, utils });
+    res.render('home', { title, status, users, utils });
+});
+
+app.get('/product', async (req: Request, res: Response) => {
+    res.render('product');
+});
+
+app.get('/admin', async (req: Request, res: Response) => {
+    res.render('admin/dashboard');
 });
 
 app.listen(port, () => {
