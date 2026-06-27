2d10de941c8cfc0abb4c43076fee28404fbb5541
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-06-27 17:40:14 +0700

    3 quy tắc cốt lõi khi quản lý TTL

--- express-typescript/src/constants/cache.constant.ts
+++ express-typescript/src/constants/cache.constant.ts
@@ -53,3 +53,31 @@ export const CACHE = {
     },
   },
 };
+
+/**
+ * Quy tắc TTL
+ *
+ * 1. Độ biến động dữ liệu
+ *   - Dữ liệu tĩnh: cấu hình hệ thống, danh mục, menu, danh sách tỉnh/thành phố, danh sách quốc gia, danh sách loại sản phẩm, danh sách loại người dùng, ...
+ *    + LONG/VERY_LONG/WEEK
+ *    + Chiến lược: Xóa bằng tag khi cập nhật dữ liệu
+ *
+ *   - Dữ liệu bán tính: User profile, danh sách sản phẩm, danh sách bài viết, chi tiết sản phẩm
+ *    + TTL: MEDIUM/LONG (1 giờ, vài giờ)
+ *    + Chiến lược: xóa bằng tag khi update
+ *
+ *   - Dữ liệu biến động cao: giá sản phẩm, số lượng tồn kho, số lượng đơn hàng, lượt xem bài viết, lượt xem sản phẩm, ...
+ *   + TTL: TINY/SHORT (1 phút, 5 phút)
+ *    + Chiến lược: Để tự động hết hạn, không cần xóa bằng tag -> dễ gây áp lực cho redis
+ *
+ * 2. Phân cấp TTL
+ *   Nếu dùng Cache Tags:
+ *   - List key: TTL ngắn
+ *   - Detail key: TTL dài hơn
+ *  => List key ngắn hơn detail key để khi có update dữ liệu, list key sẽ bị xóa trước, detail key vẫn còn -> tránh tình trạng list key bị xóa nhưng detail key vẫn còn dữ liệu cũ
+ *
+ * 3. Không để cache hết hạn cùng lúc
+ *   - Nếu có nhiều key cache hết hạn cùng thì sẽ ảnh hưởng đến hiệu năng của hệ thống, vì khi cache hết hạn, hệ thống sẽ phải truy vấn lại dữ liệu từ database
+ *
+ * const jitter = Math.flor(Math.random() * 3600); --> 0 - 5 phút
+ */
