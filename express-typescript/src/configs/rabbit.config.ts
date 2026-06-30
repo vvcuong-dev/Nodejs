c60cfc5aea635b5e0340dc253416482999b4185f
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-06-30 19:13:59 +0700

    Xây dựng Utils kết nối RabbitMQ

---
+++ express-typescript/src/configs/rabbit.config.ts
@@ -0,0 +1,6 @@
+export const rabbitConfig = {
+  host: process.env.RABBITMQ_HOST,
+  port: process.env.RABBITMQ_PORT,
+  username: process.env.RABBITMQ_USERNAME,
+  password: process.env.RABBITMQ_PASSWORD,
+};
---
+++ express-typescript/src/utils/rabbitmq.ts
@@ -0,0 +1,27 @@
+import amqp, { Channel, ChannelModel } from "amqplib";
+import { rabbitConfig } from "../configs/rabbit.config";
+
+type RabbitMQClient = {
+  connection: Promise<ChannelModel | void> | null; // thêm void là bởi vì amqp.connect có thể trả về void nếu kết nối thất bại
+  channel: Channel | null;
+  getInstance: () => RabbitMQClient;
+};
+
+export const rabbitmqClient: RabbitMQClient = {
+  connection: null,
+  channel: null,
+  getInstance() {
+    const url = `amqp://${rabbitConfig.username}:${rabbitConfig.password}@${rabbitConfig.host}:${rabbitConfig.port}/`;
+    if (!this.connection) {
+      this.connection = amqp.connect(url).then(async (conn) => {
+        if (!this.connection) {
+          this.channel = await conn.createChannel();
+        }
+
+        console.log("RabbitMQ connection established");
+      });
+    }
+
+    return this;
+  },
+};
